097cf4c98cdb1d509b7df60d2f46b3502e6ba178
Prasoon <142042273+PrasoonCr7@users.noreply.github.com>
2025-07-30 16:21:22 +0530

    Add files via upload

---
+++ backend.py
@@ -0,0 +1,57 @@
+# # Step1: Setup Pydantic model (Schema Validation)
+import uvicorn
+# Step1: Setup Pydantic model (Schema Validation)
+from pydantic import BaseModel
+from typing import List
+from fastapi import FastAPI
+from ai_agent import get_response_from_ai_agent  # Must return response string!
+
+class RequestState(BaseModel):
+    model_name: str
+    model_provider: str
+    system_prompt: str
+    messages: List[str]
+    allow_search: bool  # web search
+
+# Allowed models for validation
+ALLOWED_MODEL_NAMES = [
+    "llama3-70b-8192",
+    "mixtral-8x7b-32768",
+    "llama-3.3-70b-versatile",
+    "gpt-4o-mini"
+]
+
+# Step2: Create FastAPI app
+app = FastAPI(title="LangGraph AI Agent")
+
+@app.post("/chat")
+def chat_endpoint(request: RequestState):
+   # return {"response": "This is working!"}
+    """
+    API Endpoint to interact with the Chatbot using LangGraph and search tools.
+    It dynamically selects the model specified in the request.
+    """
+    if request.model_name not in ALLOWED_MODEL_NAMES:
+        return {"error": "Invalid model name. Kindly select the valid AI model"}
+
+    # Combine list of messages into a single query string
+    combined_query = "\n".join(request.messages)
+
+    try:
+        # Get AI Agent response
+        response = get_response_from_ai_agent(
+            llm_id=request.model_name,
+            query=combined_query,
+            allow_search=request.allow_search,
+            system_prompt=request.system_prompt,
+            provider=request.model_provider
+        )
+        return {"response": response}
+
+    except Exception as e:
+        return {"error": f"Something went wrong: {str(e)}"}
+
+# Step3: Run app
+if __name__ == "__main__":
+    
+    uvicorn.run(app, host="127.0.0.1", port=8000)
